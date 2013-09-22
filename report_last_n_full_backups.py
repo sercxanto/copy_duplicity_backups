68c18375346e62d96daca9bbd95b2fc6bddca21d
Georg Lutz <georg@georglutz.de>
2013-09-22 22:55:47 +0200

    Cleanup get_duplicity_files and unit tests
    Change-UUID: ce11734020f44f2a9e0787db206a4112

--- report_last_n_full_backups.py
+++ report_last_n_full_backups.py
@@ -24,7 +24,11 @@ import argparse
 import datetime
 import os
 import re
+import shutil
 import sys
+import unittest
+import tempfile
+
 
 def get_args():
     '''Configures command line parser and returns parsed parameters'''
@@ -36,6 +40,45 @@ def get_args():
 
     return parser.parse_args()
 
+
+def ts_regex(number):
+    '''Returns timestamp regex, matching group with given numberr, e.g.
+    "timestamp1" or "timestamp2" '''
+    if number != None:
+        return "(?P<timestamp" + str(number) + ">\d{8}T\d{2}\d{4}[A-Z])"
+    else:
+        return "(?P<timestamp>\d{8}T\d{2}\d{4}[A-Z])"
+
+
+def get_unix_timestamp(timestamp):
+    '''Returns a unix timestamp for given textual duplicity timestamp'''
+    time = datetime.datetime.strptime(timestamp, "%Y%m%dT%H%M%SZ")
+    return (time - datetime.datetime(1970, 1 , 1)).total_seconds()
+
+
+def add_entry(dup_files, timestamp, is_full, filename):
+    '''Adds an entry to the dup_files dictionnary.
+    Either a new entry with timestamp is created or an existing is updated.
+
+    Example:
+
+        dup_files = {"123456789" :
+            { "is_full": True,
+              "files": ["file1", "file2", "file3"] } }
+
+    Raises an exception on error'''
+
+    if dup_files.has_key(timestamp):
+        assert dup_files[timestamp]["is_full"] == is_full
+        if dup_files[timestamp].has_key("files"):
+            dup_files[timestamp]["files"].append(filename)
+        else:
+            dup_files[timestamp]["files"] = [filename]
+    else:
+        entry = {}
+        entry["is_full"] = is_full
+        entry["files"] = [filename]
+        dup_files[timestamp] = entry
 
 
 def get_duplicity_files(directory):
@@ -52,36 +95,6 @@ def get_duplicity_files(directory):
             duplicity-inc.timestamp1.to.timestamp2.volnr.difftar.gpg
             duplicity-new-signatures.timestamp1.to.timestamp2.sigtar.gpg
        timestamp example: 20130126T070058Z'''
-
-    def ts_regex(number):
-        '''Returns timestamp regex, matching group with given numberr, e.g.
-        "timestamp1" or "timestamp2" '''
-        if number != None:
-            return "(?P<timestamp" + str(number) + ">\d{8}T\d{2}\d{4}[A-Z])"
-        else:
-            return "(?P<timestamp>\d{8}T\d{2}\d{4}[A-Z])"
-
-    def get_python_timestamp(timestamp):
-        '''Returns a python timestamp for given textual duplicity timestamp'''
-        time = datetime.datetime.strptime(timestamp, "%Y%m%dT%H%M%SZ")
-        return (time - datetime.datetime(1970, 1 , 1)).total_seconds()
-
-    def add_entry(dup_files, timestamp, is_full, filename):
-        '''Adds an entry to the dup_files dictionnary. Raises an exception
-        on error'''
-
-        if dup_files.has_key(timestamp):
-            assert dup_files[timestamp]["is_full"] == is_full
-            if dup_files[timestamp].has_key("files"):
-                dup_files[timestamp]["files"].append(filename)
-            else:
-                dup_files[timestamp]["files"] = [filename]
-        else:
-            entry = {}
-            entry["is_full"] = is_full
-            entry["files"] = [filename]
-            dup_files[timestamp] = entry
-
 
     full_prefix = "duplicity\-full\." + ts_regex(None) + "\."
     full_manifest = re.compile(full_prefix + "manifest\.gpg")
@@ -100,37 +113,37 @@ def get_duplicity_files(directory):
     for name in all_files:
         result = full_manifest.match(name)
         if result != None :
-            timestamp = get_python_timestamp(result.group("timestamp"))
+            timestamp = get_unix_timestamp(result.group("timestamp"))
             add_entry(dup_files, timestamp, True, name)
             continue
 
         result = full_difftar.match(name)
         if result != None:
-            timestamp = get_python_timestamp(result.group("timestamp"))
+            timestamp = get_unix_timestamp(result.group("timestamp"))
             add_entry(dup_files, timestamp, True, name)
             continue
 
         result = full_signatures.match(name)
         if result != None:
-            timestamp = get_python_timestamp(result.group("timestamp"))
+            timestamp = get_unix_timestamp(result.group("timestamp"))
             add_entry(dup_files, timestamp, True, name)
             continue
         
         result = inc_manifest.match(name)
         if result != None:
-            timestamp = get_python_timestamp(result.group("timestamp2"))
+            timestamp = get_unix_timestamp(result.group("timestamp2"))
             add_entry(dup_files, timestamp, False, name)
             continue
         
         result = inc_difftar.match(name)
         if result != None:
-            timestamp = get_python_timestamp(result.group("timestamp2"))
+            timestamp = get_unix_timestamp(result.group("timestamp2"))
             add_entry(dup_files, timestamp, False, name)
             continue
         
         result = inc_signatures.match(name)
         if result != None:
-            timestamp = get_python_timestamp(result.group("timestamp2"))
+            timestamp = get_unix_timestamp(result.group("timestamp2"))
             add_entry(dup_files, timestamp, False, name)
             continue
 
@@ -166,7 +179,34 @@ def main():
     for file_ in sorted(files):
         print file_
 
+
 if __name__ == "__main__":
     main()
 
+
+
+class TestAll(unittest.TestCase):
+    '''Test of the module in general'''
+
+    @staticmethod
+    def gen_tempfolder():
+        '''Returns temporary folder name'''
+        return tempfile.mkdtemp(prefix="tmp_report_last_n")
+
+    @staticmethod
+    def add_file(folder, filenames):
+        '''Adds an empty filename to folder '''
+        for filename in filenames:
+            path = os.path.join(folder, filename)
+            with open(path, mode="wb"):
+                pass
+
+    def test_01(self):
+        '''Dummy test'''
+        folder = self.gen_tempfolder()
+        names = [ "duplicity-full.20130101T010000Z.manifest.gpg" ]
+        
+        self.add_file(folder, names)
+
+        shutil.rmtree(folder)
 
